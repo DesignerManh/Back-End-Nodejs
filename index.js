004e8430ad6c771a20100c083d1c8e91fd7b4f79
Trần Duy Mạnh <manhhoc2k4vv@gmail.com>
2024-12-13 20:36:00 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+
 const route = require("./routes/client/index.route"); // Nhúng code ở file index.route.js
 
+
+const port = process.env.PORT;
 // Cấu hình thằng pug vào trong dự án
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -9,6 +13,6 @@ app.set("view engine", "pug");
 // Nhúng code ở file index.route.js
 route(app);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(` Server is running on port ${port} `);
 });
