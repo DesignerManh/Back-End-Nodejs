57247461779680f952c6ee57d7ec09d70e2fdc3f
Trần Duy Mạnh <manhhoc2k4vv@gmail.com>
2024-12-14 23:43:55 +0700

    Làm phần tìm kiếm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,25 +1,51 @@
 // Bộ lọc
 const boxFilter = document.querySelector("[box-filter]");
-if(boxFilter) {
+if (boxFilter) {
   let url = new URL(location.href); // Nhân bản url
 
   // Bắt sự kiện onChange
   boxFilter.addEventListener("change", () => {
     const value = boxFilter.value;
-    
-    if(value) {
+
+    if (value) {
       url.searchParams.set("status", value);
     } else {
       url.searchParams.delete("status");
     }
 
     location.href = url.href;
-  })
+  });
 
   // Hiển thị lựa chọn mặc định
   const statusCurrent = url.searchParams.get("status");
-  if(statusCurrent) {
+  if (statusCurrent) {
     boxFilter.value = statusCurrent;
   }
 }
 // Hết Bộ lọc
+
+// Tìm kiếm
+const formSearch = document.querySelector("[form-search]");
+if (formSearch) {
+  let url = new URL(location.href); // Nhân bản url
+
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định: submit form
+    const value = formSearch.keyword.value;
+
+    if (value) {
+      url.searchParams.set("keyword", value);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+
+    location.href = url.href;
+  });
+
+  // Hiển thị từ khóa mặc định
+  const valueCurrent = url.searchParams.get("keyword");
+  if (valueCurrent) {
+    formSearch.keyword.value = valueCurrent;
+  }
+}
+// Hết Tìm kiếm
