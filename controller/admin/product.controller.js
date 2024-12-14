fed252d90865b7e92682211e8f5b8bbe1b0002cc
Trần Duy Mạnh <manhhoc2k4vv@gmail.com>
2024-12-15 00:08:54 +0700

    Nâng cấp tính năng tìm kiếm

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,6 +1,5 @@
 const Product = require("../../models/product.model");
 
-// [GET] /admin/product
 module.exports.index = async (req, res) => {
   const find = {
     deleted: false
@@ -11,6 +10,13 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
   // Hết Lọc theo trạng thái
+
+  // Tìm kiếm
+  if(req.query.keyword) {
+    const regex = new RegExp(req.query.keyword, "i");
+    find.title = regex;
+  }
+  // Hết Tìm kiếm
 
   const products = await Product.find(find);
 
