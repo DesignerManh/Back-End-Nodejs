07e9ecd652a18e8bdd9cec9ea7a7b2a590ae90e0
Trần Duy Mạnh <manhhoc2k4vv@gmail.com>
2024-12-14 22:03:31 +0700

    Xây dựng layout chung

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const port = process.env.PORT;
 const database = require("./config/database");
 database.connect();
 
+const systemconfig = require("./config/system");
 const routeClient = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
 
 app.set("views", "./views"); // Tìm đến thư mục tên là views
 app.set("view engine", "pug"); // template engine sử dụng: pug
 
+app.locals.prefixAdmin = systemconfig.prefixAdmin;
 app.use(express.static("public")); // Thiết lập thư mục chứa file tĩnh
 
 routeClient(app);
